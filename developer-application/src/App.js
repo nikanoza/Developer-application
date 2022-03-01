d8e16e8f02770a84b31aad49ed8e54d26c7f2095
nikanoza <nikanozadze1010@gmail.com>
2022-03-01 09:54:22 +0400

    setting up, components

---
+++ developer-application/src/App.js
@@ -0,0 +1,13 @@
+import Main from './components/main/Main'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ developer-application/src/components/main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Main.module.css';
+
+const Main = () => {
+ return <div>
+     main works!
+ </div>
+}
+
+export default Main
